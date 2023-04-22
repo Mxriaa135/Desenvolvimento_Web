f958ac699e5e857eed93b406d6219511e0b062c4
Mxriaa135 <124539047+Mxriaa135@users.noreply.github.com>
2023-04-22 09:52:06 -0300

    Update javaScript.js

--- Lista_4/html/javaScript.js
+++ Lista_4/html/javaScript.js
@@ -1,4 +1,4 @@
 var tagnome = document.getElementById("nome")
-tagnome.innerHTML = "Lewis Hamilton"
+tagnome.innerHTML = "Morilo"
 var tagtexto = document.getElementById("texto")
-tagtexto.innerHTML = "É um dos melhores pilotos de corrida de Formula 1 <br> Com 7 títulos"
+tagtexto.innerHTML = "É um dos melhores pilotos de corrida de Formula 1 <br> Com 12 títulos"
