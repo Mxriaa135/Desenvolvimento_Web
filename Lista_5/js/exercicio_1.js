254b3dd144c043178b28e1e7fa4195288cbd9e96
Mxriaa135 <124539047+Mxriaa135@users.noreply.github.com>
2023-04-26 22:50:26 -0300

    Modulo II

---
+++ Lista_5/js/exercicio_1.js
@@ -0,0 +1,40 @@
+var listaPessoas = [
+    pessoa1 = {
+        nome: "Helena",
+        idade: 16,
+        ehAdmin: false,
+        email: "helena123@gmail.com"
+    },
+    pessoa2 = {
+        nome: "Tiago",
+        idade: 18,
+        ehAdmin: false,
+        email: "tiago567@gmail.com"
+    },
+    pessoa3 = {
+        nome: "Alonso",
+        idade: 45,
+        ehAdmin: true,
+        email: "alonso678@gmail.com"
+    },
+    pessoa4 = {
+        nome: "Roberto",
+        idade: 34,
+        ehAdmin: false,
+        email: "robert012@gmail.com"
+    },
+    pessoa5 = {
+        nome: "Ana",
+        idade: 21,
+        ehAdmin: false,
+        email: "ana543@gmail.com"
+    }
+]
+for(var i = 0; i < listaPessoas.length; i++){
+    if(listaPessoas[i].ehAdmin){
+        console.log("Olá " + listaPessoas[i].nome + "! Bem vindo(a) você é admin do sistema, enviamos um email para " + listaPessoas[i].email + " para você criar uma senha")
+    }
+    else{
+        console.log("Olá " + listaPessoas[i].nome + "! Bem vindo(a) você não é admin do sistema, enviamos um email para " + listaPessoas[i].email + " para você criar uma senha")
+    }
+}
